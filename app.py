ed672e554cbeea43edf48150b628016ea83b9b04
PredictRAM DataHub <95133921+PredictRAM-Org@users.noreply.github.com>
2023-11-29 00:49:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,60 +1,38 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-import os
 
-# Function to load stock data from the "Stock_Data" folder
-def load_stock_data():
-    stock_data = {}
-    folder_path = "Stock_Data"
+# Load data from the Excel file
+file_path = "IIP_Data.xlsx"
+df = pd.read_excel(file_path)
 
-    # Loop through all files in the folder
-    for file_name in os.listdir(folder_path):
-        if file_name.endswith(".xlsx"):
-            stock_name = os.path.splitext(file_name)[0]
-            file_path = os.path.join(folder_path, file_name)
-            stock_data[stock_name] = pd.read_excel(file_path)
-
-    return stock_data
-
-# Function to plot separate and combined graphs
-def plot_graphs(stock_data, selected_stock):
-    # Check if 'Industry' is present in the stock data
-    if 'Industry' not in stock_data:
-        st.error("Error: 'Industry' stock data not found.")
-        return
-
-    # Check if the selected stock is present in the stock data
-    if selected_stock not in stock_data:
-        st.error(f"Error: Stock data for {selected_stock} not found.")
-        return
-
-    # Plot separate graph for industry
-    fig_industry = px.line(stock_data['Industry'], x="Date", y="Close", title="Industry Close Price")
-    st.plotly_chart(fig_industry)
-
-    # Plot separate graph for the selected stock
-    fig_selected_stock = px.line(stock_data[selected_stock], x="Date", y="Close", title=f"{selected_stock} Close Price")
-    st.plotly_chart(fig_selected_stock)
-
-    # Plot combined graph for industry and selected stock
-    fig_combined = px.line(stock_data[selected_stock], x="Date", y="Close", title=f"Industry and {selected_stock} Close Price")
-    fig_combined.add_trace(px.line(stock_data['Industry'], x="Date", y="Close", line_dash="dash", name="Industry").data[0])
-    st.plotly_chart(fig_combined)
-
-# Load stock data
-stock_data = load_stock_data()
+# Convert "Date" column to datetime format
+df["Date"] = pd.to_datetime(df["Date"], format="%Y-%m-%d", errors='coerce')
 
 # Streamlit app
-st.title("Stock Data Visualization App")
+st.title("IIP Data Visualization App")
 
 # Sidebar
 st.sidebar.header("Select Options")
+selected_column = st.sidebar.selectbox("Select Column", df.columns[1:])
 
-# Check if 'Industry' is present before displaying the option
-if 'Industry' in stock_data:
-    selected_stock = st.sidebar.selectbox("Select Stock", ['Industry'] + list(stock_data.keys()), index=1)
-    # Plot graphs
-    plot_graphs(stock_data, selected_stock)
-else:
-    st.sidebar.warning("Warning: 'Industry' stock data not found.")
+# Default start date
+default_start_date = pd.to_datetime("2012-04-01")
+
+# Date input widget with default value
+start_date = st.sidebar.date_input("Select Start Date", default_start_date)
+
+# Ensure that the start_date is a Timestamp object
+start_date = pd.to_datetime(str(start_date))  # Convert to Timestamp
+
+# Filter data based on user selection
+filtered_df = df[df["Date"].dt.date >= start_date.date()]
+
+# Plot the selected column
+fig = px.line(filtered_df, x="Date", y=selected_column, title=f"{selected_column} over Time")
+st.plotly_chart(fig)
+
+# Display data table for selected date and columns
+st.header("Selected Data Table")
+selected_data = filtered_df.loc[filtered_df["Date"].dt.date == start_date.date(), [selected_column]]
+st.dataframe(selected_data)
